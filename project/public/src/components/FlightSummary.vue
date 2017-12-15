28d45d78ba28a8efc0c9b25a3d9056357748639b
Andrew <andrewhuh67@gmail.com>
2017-12-14 16:38:41 -0800

    changed airline text to show airline name, not code

--- project/public/src/components/FlightSummary.vue
+++ project/public/src/components/FlightSummary.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-
+		<!-- <p>{{flight}}</p> -->
 		<p>City: {{flight.FlightSegment[0].ArrivalAirport.LocationCode}}</p>
-		<p>Airline: {{flight.Segmentid}}</p>
-		<p>Price: {{flight.Segmentid}}</p>
+		<p>Airline: {{flight.FlightSegment[0].OperatedByAirline.CompanyText}}</p>
+		<p>Price: {{flight.FlightSegment[0].ArrivalAirport}}</p>
 		<!-- <router-link v-bind:to="{name: 'flight', params: {id: flight.id}}">Flight Detail</router-link> -->
 	</div>
 </template>
