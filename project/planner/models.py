191899b16e3f259e5d3dee01b025370ade7be3bb
Jimmy Jordan <jim.j.jordan89@gmail.com>
2018-01-26 15:13:52 -0500

    Updating everything to master

---
+++ project/planner/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class SavedFlightRoute(models.Model):
+
+	trip_type = models.CharField(max_length=10)
+	destination = models.CharField(max_length=4)
+	origin = models.CharField(max_length=4)
+	passengers = models.PositiveSmallIntegerField()
+	# departure_date = models.DateField()
+	# return_date = models.DateField()
+	user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+class DepartureEstimate(models.Model):
+
+	airline = models.CharField(max_length=50)
+	estimated_roundtrip = models.FloatField()
+	flight_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+	search_timestamp = models.DateTimeField(default=timezone.now)
+	direct_flight = models.BooleanField()
+	flight_route = models.ForeignKey(SavedFlightRoute, on_delete=models.PROTECT)
+
+class ReturnEstimate(models.Model):
+	airline = models.CharField(max_length=50)
+	estimated_roundtrip = models.FloatField()
+	flight_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+	search_timestamp = models.DateTimeField(default=timezone.now)
+	direct_flight = models.BooleanField()
+	flight_route = models.ForeignKey(SavedFlightRoute, on_delete=models.PROTECT)
+
